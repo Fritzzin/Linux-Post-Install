fd49dd232e6015b292355241fe347b55cd347f69
Augusto Fritz <augustofritzz@gmail.com>
2022-09-26 11:58:45 -0300

    Updated scripts and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # linuxinstall
+These are some basic scripts to setup my debian based distros environment alongside the [dotfiles](https://github.com/Fritzzin/dotfiles) repo. 
+
+## install.sh
+Updates the repository and install the basic packages that I need.
+
+## update.sh
+Updates the system via apt.
+
+## clean.sh
+Uses maintenance commands to keep apt clean such as autoremove and autoclean.
+
+## monoaudio.sh
+One of my laptop speakers died, so this is a solution I've found for mono audio.
---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+# Variables
+SCRIPTS_FOLDER="$HOME/.scripts"
+GREEN=$'\e[0;32m'
+
+# Update repository and system
+echo "\n\nUPDATING REPOSITORY AND INSTALLING UPDATES"
+sudo apt update && sudo apt upgrade -y
+
+# Install packages
+echo "\n\nINSTALLING PACKAGES"
+# wget curl
+sudo apt install -y wget \
+                    curl \
+                    openssh-client \
+                    build-essential \
+                    default-jdk \
+                    default-jre \
+                    vim \
+                    git \
+                    nodejs \
+                    npm \
+                    luarocks \
+                    tlp \
+                    tlp-rdw \
+                    ufw \
+                    timeshift \
+                    vlc \
+                    unzip \
+                    gzip \
+                    p7zip-full \
+                    p7zip-rar \
+                    unrar \
+                    unrar-free \
+                    neofetch \
+                    gimp
+
+
+echo "\n\nENABLE FIREWALL"
+sudo ufw enable
+sudo systemctl enable ufw
+
+
+echo "\n\nENABLE TLP"
+sudo tlp start
+sudo systemctl enable tlp
+
+# Creating bash_aliases file
+echo "\n\nCREATING BASH ALIASES"
+touch ~/.bash_aliases
+echo "
+# BASH
+alias bashrc=\"nano ${HOME}/.bashrc\"
+alias aliases=\"nano ${HOME}/.bash_aliases\"
+
+# MAINTENANCE
+alias update=\"${SCRIPTS_FOLDER}/update.sh\"
+alias clean=\"${SCRIPTS_FOLDER}/clean.sh\"
+
+# DOTFILES
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+" >> ~/.bash_aliases
+
+# Creating .scripts folder
+echo "\n\nCREATING SCRIPTS FOLDER AND MOVING SCRIPTS"
+mkdir $SCRIPTS_FOLDER
+chmod +x *.sh
+mv update.sh $SCRIPTS_FOLDER
+mv clean.sh $SCRIPTS_FOLDER
+mv monoaudio.sh $SCRIPTS_FOLDER
+
+
+echo "\n\nDONE \n\n"
